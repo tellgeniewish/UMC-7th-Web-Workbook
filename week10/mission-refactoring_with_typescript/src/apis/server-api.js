29eda0259a33eb1dc998e0d5a701a6ef8c23df5e
tellgeniewish <jadastras2@gmail.com>
2025-01-09 17:42:26 +0900

    week10 mission delete

--- week10/mission-refactoring_with_typescript/src/apis/server-api.js
+++
@@ -1,8 +0,0 @@
-// // src/apis/server-api.js
-// import axios from "axios";
-
-// const serverApi = axios.create({
-//     baseURL: import.meta.env.VITE_SERVER_API_URL,
-// })
-
-// export {serverApi}
--- week10/mission-refactoring_with_typescript/src/pages/not-found.jsx
+++
@@ -1,8 +0,0 @@
-// src/pages/not-found.jsx
-const NotFound = () => {
-    return (
-        <h2>너는 찾을 수 없는 페이지 야호~!</h2>
-    );
-};
-
-export default NotFound;
