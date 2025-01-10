547b2b6351455022ebd0a5c774f73d7fa5926c4d
tellgeniewish <jadastras2@gmail.com>
2025-01-11 08:48:42 +0900

    mission delete unused code

--- week10/mission-refactoring_with_typescript/src/pages/home.jsx
+++ week10/mission-refactoring_with_typescript/src/pages/home.jsx
@@ -2,14 +2,12 @@
 import {MOVIES} from '../mocks/movies';
 import ShowMovies from "../components/ShowMovies";
 import styled from 'styled-components';
-import SearchMovieList from '../Movie/search-movie-list';
 
 const HomePage = () => {
     return (
         //<h2>Home Page 야호~!</h2>
         <HomeContainer>
             <ShowMovies movies={MOVIES?.results}/>
-            <SearchMovieList/>
         </HomeContainer>
     );
 };
