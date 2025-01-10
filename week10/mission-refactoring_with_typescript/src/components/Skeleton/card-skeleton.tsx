eb3270b908e4665975c0f89f4e9663ee4cbc9815
tellgeniewish <jadastras2@gmail.com>
2025-01-11 05:02:54 +0900

    mission components/Skeleton/card-skeleton.jsx to .tsx

--- week10/mission-refactoring_with_typescript/src/components/Skeleton/card-skeleton.tsx
+++ week10/mission-refactoring_with_typescript/src/components/Skeleton/card-skeleton.tsx
@@ -1,15 +1,17 @@
-// src/components/Skeleton/card-skeleton.jsx
+// src/components/Skeleton/card-skeleton.tsx
+import React from 'react';
 import * as S from './card-skeleton.style'
 
-const CardSkeleton = () => {
+// const CardSkeleton = () => {
+const CardSkeleton: React.FC = () => {
     return (
-        <S.SkeletonConatiner>
+        <S.SkeletonContainer>
             <S.CardMain/>
             <S.CardTextWrapper>
                 <S.TitleBox></S.TitleBox>
                 <S.DescriptBox></S.DescriptBox>
             </S.CardTextWrapper>
-        </S.SkeletonConatiner>
+        </S.SkeletonContainer>
     )
 };
 
