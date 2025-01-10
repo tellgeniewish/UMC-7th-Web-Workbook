8739e00857424480119e8394e91422c4439420c5
tellgeniewish <jadastras2@gmail.com>
2025-01-11 06:07:45 +0900

    commenting out unused code

--- week10/mission-refactoring_with_typescript/src/hooks/useCustomFetch.js
+++ week10/mission-refactoring_with_typescript/src/hooks/useCustomFetch.js
@@ -1,31 +1,31 @@
-// src/hooks/useCustomFetch.js
-import { useEffect, useState } from "react";
-import {axiosInstance} from "../apis/axios-instance";
+// // src/hooks/useCustomFetch.js
+// import { useEffect, useState } from "react";
+// import {axiosInstance} from "../apis/axios-instance";
 
-// const {data, isLoading, isError} = useCustomFetch('url');
+// // const {data, isLoading, isError} = useCustomFetch('url');
 
-const useCustomFetch = (url) => {
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isError, setIsError] = useState(false);
+// const useCustomFetch = (url) => {
+//     const [data, setData] = useState([]);
+//     const [isLoading, setIsLoading] = useState(false);
+//     const [isError, setIsError] = useState(false);
     
-    useEffect(() => {
-        const fetchData = async () => {
-            setIsLoading(true);
-            try {
-                const response = await axiosInstance.get(url);
-                setData(response.data);
-            } catch (error) {
-                console.error("useCustomFetch error:", error);
-                setIsError(true);
-            } finally {
-                setIsLoading(false);
-            }
-        }
-        fetchData();
-    }, [url]);
+//     useEffect(() => {
+//         const fetchData = async () => {
+//             setIsLoading(true);
+//             try {
+//                 const response = await axiosInstance.get(url);
+//                 setData(response.data);
+//             } catch (error) {
+//                 console.error("useCustomFetch error:", error);
+//                 setIsError(true);
+//             } finally {
+//                 setIsLoading(false);
+//             }
+//         }
+//         fetchData();
+//     }, [url]);
 
-    return {data, isLoading, isError}
-}
+//     return {data, isLoading, isError}
+// }
 
-export default useCustomFetch;
+// export default useCustomFetch;
