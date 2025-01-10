06b8b648657c249a29e8a3eb8258e8797fc23098
tellgeniewish <jadastras2@gmail.com>
2025-01-11 07:20:10 +0900

    mission03 delete unused code

--- week08/mission03-refactoring_usemutation/src/pages/popular.jsx
+++ week08/mission03-refactoring_usemutation/src/pages/popular.jsx
@@ -87,7 +87,6 @@ const PopularPage = () => {
                 {/* {isFetching && <ClipLoader color={'deeppink'}/>} */}
             {/* </div> */}
             <ShowMovies movies={movies?.results}/>
-            <SearchMovieList/>
             <PaginationContainer>
                 <PageBtn onClick={PrevPage} disabled={currentPage === 1}>이전</PageBtn>
                 <CurrentPage>{currentPage}페이지</CurrentPage>
